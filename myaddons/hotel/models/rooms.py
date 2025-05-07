8f0165064c2f0d619887e6d203eb5d4e89409052
GGarrd <150632575+GGarrd@users.noreply.github.com>
2025-05-07 14:16:52 +0800

    Add files via upload

--- myaddons/hotel/models/rooms.py
+++ myaddons/hotel/models/rooms.py
@@ -3,10 +3,9 @@ from odoo import models, fields, api
 
 class Rooms(models.Model):
     _name = 'hotel.rooms'
-    _description = 'Rooms'
+    _description = 'Hotel Rooms'
 
     name = fields.Char(string="Room No.", required=True)
     description = fields.Char(string="Room Description")
-    room_type_id = fields.Many2one('hotel.roomtypes', string='Room Type')
-    price = fields.Float(string="Price", required=True)
-    is_available = fields.Boolean(string="Is Available", default=True)
+    roomtype_id = fields.Many2one('hotel.roomtypes', string="Room Type", required=True)
+    roomtype_name = fields.Char(string="Room Type Name", related='roomtype_id.name', store=True, readonly=True)
